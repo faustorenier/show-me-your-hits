91b4eb2ce7c9d2a4c80eb80cbb174b23b48f1b4e
Fausto Renier <fausto.renier@gmail.com>
2019-04-26 15:32:03 +0200

    mobile fix

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -17,19 +17,26 @@ const Modal = ({ data, close, isVisible }) => {
         <PoseGroup>
             {isVisible && [
                 <Pose.modal.bg key="anim-modal-shade" className="c__Modal_bg" onClick={handleBgClick} />,
-                <Pose.modal.content key="anim-modal-content" className="c__Modal_cont">
-                    <div className="c__Modal_cont_wrap">
-                        <div className="c__Modal_cont_close" onClick={close}>
-                            <div className="c__Modal_cont_close_rect"></div>
-                            <div className="c__Modal_cont_close_rect"></div>
+
+                <Pose.modal.content key="anim-modal-content" className="c__Modal_anim">
+                    <div className="c__Modal_anim_wrap">
+
+                        <div className="c__Modal_cont">
+                            <div className="c__Modal_cont_wrap">
+                                <div className="c__Modal_cont_close" onClick={close}>
+                                    <div className="c__Modal_cont_close_rect"></div>
+                                    <div className="c__Modal_cont_close_rect"></div>
+                                </div>
+                                <h1>{message}</h1>
+                                <div className="c__Modal_cont_img">
+                                    {image && <img src={image} alt="Playlist Cover" />}
+                                </div>
+                                <a href={link}>
+                                    <Button title={cta} />
+                                </a>
+                            </div>
                         </div>
-                        <h1>{message}</h1>
-                        <div className="c__Modal_cont_img">
-                            {image && <img src={image} alt="Playlist Cover" />}
-                        </div>
-                        <a href={link}>
-                            <Button title={cta} />
-                        </a>
+
                     </div>
                 </Pose.modal.content>
             ]}
