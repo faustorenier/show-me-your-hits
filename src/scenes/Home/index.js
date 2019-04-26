b305822cdd7e72ad63fd061ef848ac48c780607d
Fausto Renier <fausto.renier@gmail.com>
2019-04-26 16:02:29 +0200

    div100

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -5,8 +5,6 @@ import * as Pose from "../../utils/poses";
 import keys from "../../utils/poses/keys.json";
 import "./styles.css";
 
-import Div100vh from "react-div-100vh";
-
 class Home extends Component {
 
     constructor(props) {
@@ -28,12 +26,10 @@ class Home extends Component {
     render() {
         const { showLogin } = this.props;
         return (
-            <Div100vh>
-                <Pose.Main key={keys.scenes.home} pose={this.state.isVisible ? "visible" : "hidden"} className="page" id="home">
-                    {showLogin && <Logger handleLogin={this.handleLogin} />}
-                    {!showLogin && <Logged />}
-                </Pose.Main>
-            </Div100vh>
+            <Pose.Main key={keys.scenes.home} pose={this.state.isVisible ? "visible" : "hidden"} className="page" id="home">
+                {showLogin && <Logger handleLogin={this.handleLogin} />}
+                {!showLogin && <Logged />}
+            </Pose.Main>
         );
     }
 }
